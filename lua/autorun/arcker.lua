2d59d102edeef3e8a79b64e755060fe855ff5cae
Pukki <plurily@gmail.com>
2017-04-26 21:07:40 -0300

    Init

--- lua/autorun/arcker.lua
+++ lua/autorun/arcker.lua
@@ -18,46 +18,146 @@ if SERVER then
 			end
 		end
 	end
-	local sv = 1 		// Server Side
-	local cs = 2		// Client Side
-	local sh = sv + cs	// Shared
-	local Files = {
-		{ 'net', sh },
-		{ 'core', sh },
-	}
-	Arcker.ServerFiles = {}
-	Arcker.ClientFiles = {}
-	for k, v in ipairs( Files ) do
-		if v[2] ~= 0 and type(v) == 'table' then
-			local Filename = 'arcker/' .. v[1] .. '.lua'
-			local Client = bit.band( v[2], cs )
-			local Server = bit.band( v[2], sv )
-			if Server then
-				table.insert( Arcker.ServerFiles, Filename )
-				include( Filename )
+
+	Arcker.Print = {}
+	Arcker.Color =    Color(186, 186, 186)
+	Arcker.WarColor = Color(235, 123,  89)
+	Arcker.ErrColor = Color(200, 0,     0)
+	Arcker.SublimeColor = Color(110, 150, 152)
+
+	function Arcker:GetName()
+		return ("[" .. self.Name .. ' v' .. self.Version .. "]")
+	end
+
+	function Arcker:PrintA(n, ...)
+		local t = { ... }
+		local ret = ""
+		if n != 1 or n != 2 then
+			t = {n}
+		end
+		for k, v in pairs(t) do
+			if type(v) == "Player" then
+				ret = ret .. v:Nick() .. "(" .. v:SteamID() .. ")"
+			elseif type(v) == "table" then
+				ret = ret .. tostring(v)
+			else
+				ret = ret .. v
 			end
-			if Client then
-				table.insert( Arcker.ClientFiles, Filename )
-				AddCSLuaFile( Filename )
+			ret = ret .. " "
+		end
+
+
+		if n == 1 then
+			MsgC(Arcker.Color, Arcker:GetName())
+			MsgC(" ")
+			MsgC(Arcker.ErrColor, ret .. "\n")
+		elseif n == 2 then
+			MsgC(Arcker.ErrColor, "[ARCKER ERROR]")
+			MsgC(" ")
+			MsgC(Arcker.ErrColor, ret .. "\n")
+		else
+			MsgC(Arcker.Color, Arcker:GetName())
+			MsgC(" ")
+			MsgC(Arcker.Color, ret .. "\n")
+		end
+	end
+
+	function Arcker:Boot()
+		local NotLoad = {
+			//
+		}
+		local Files = {
+			sv = {},
+			cl = {},
+			sh = {}
+		}
+		local all = {}
+		local fil, fol = file.Find("arcker/*", "LUA")
+		for z, x in pairs(fol) do
+			for _, v in pairs(file.Find("arcker/" .. x .. "/*.lua", "LUA")) do
+				for k, source in pairs(file.Find("arcker/*.lua", "LUA")) do
+					table.insert(all, "arcker/" .. source)
+				end
+				table.insert(all, "arcker/" .. x .. "/" .. v)
+			end
+		end
+
+		for k, v in pairs(NotLoad) do
+			for _, al in pairs(all) do
+				if v == al then
+					table.RemoveByValue(all, al)
+				end
+			end
+		end
+
+		for _, fl in pairs(all) do
+			local read = file.Read(fl, "LUA")
+			if string.find(read, "// Server") then
+				table.insert(Files.sv, fl)
+			elseif string.find(read, "// Client") then
+				table.insert(Files.cl, fl)
+			elseif string.find(read, "// Shared") then
+				table.insert(Files.sh, fl)
+			else
+				if string.find(read, "CLIENT") and string.find(read, "SERVER") then
+					table.insert(Files.sh, fl)
+				elseif string.find(read, "CLIENT") then
+					table.insert(Files.cl, fl)
+				elseif string.find(read, "SERVER") then
+					table.insert(Files.sv, fl)
+				else
+					table.insert(Files.sh, fl)
+				end
+			end
+		end
+		
+		local function FormatPrint(var)
+			local w = string.Replace(var, "_sv", "")
+			w = string.Replace(w, "_cl", "")
+			w = string.Replace(w, "_sh", "")
+			w = string.Replace(w, ".lua", "")
+			w = string.Replace(w, "aura/", "")
+			w = string.Replace(w, "/", ".")
+			return w
+		end
+
+		local function IncludeAll()
+			for k, v in pairs(Files.sv) do
+				include(v)
+				Arcker:PrintA("Server file initialized: " .. FormatPrint(v))
+			end
+			for k, v in pairs(Files.cl) do
+				AddCSLuaFile(v)
+				table.insert(Arcker.ClientFiles, v)
+				Arcker:PrintA("Client file initialized: " .. FormatPrint(v))
+			end
+			for k, v in pairs(Files.sh) do
+				include(v)
+				AddCSLuaFile(v)
+				table.insert(Arcker.ClientFiles, v)
+				Arcker:PrintA("Shared file initialized: " .. FormatPrint(v))
+			end
+		end
+
+		IncludeAll()
+
+		function Arcker.CsInclude( ply )
+			if ply then
+				net.Start( 'arcker files' )
+				net.WriteTable( Arcker.ClientFiles )
+				net.Send( ply )
+			elseif #player.GetHumans() then
+				net.Start( 'arcker files' )
+				net.WriteTable( Arcker.ClientFiles )
+				net.Broadcast( )
 			end
 		end
 	end
-	
-	function Arcker.CsInclude( ply )
-		if ply then
-			net.Start( 'arcker files' )
-			net.WriteTable( Arcker.ClientFiles )
-			net.Send( ply )
-		elseif #player.GetHumans() then
-			net.Start( 'arcker files' )
-			net.WriteTable( Arcker.ClientFiles )
-			net.Broadcast( )
-		end
-	end
+
+	Arcker:Boot()
 	
 	hook.Add( 'PlayerAuthed', Arcker.CsInclude )
 	hook.Add( 'PlayerInitialSpawn', Arcker.CsInclude )
-	
 	
 end
 
